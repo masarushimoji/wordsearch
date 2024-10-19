66aca34685a61cabd37edc361d4d468a0608288e
maru <masarushimoji@hotmail.com>
2024-10-19 15:18:29 +0100

    redid how columns and rows are inputted

--- main.py
+++ main.py
@@ -2,22 +2,26 @@ class WordSearch(object):
     def __init__(self, grid):
         self.grid = grid
         self.fileContents = []
-        rowLength = 10
-
-
-        with open(self.grid, 'r') as file:
-            self.fileContents = file.read().split()
+        with open(self.grid, 'r') as file: 
+            for line in file:
+                self.fileContents.append(line)
+        self.columns = len(fileContents[0])
+        self.rows = len(fileContents)
         
+    
 
+    
     def is_present(self, word):
-        print(self.fileContents)
+        wordLen = len(word)
+
+        
+            
         return False
 
+  
 
 ws = WordSearch("/home/maru/Projects/pexipStuff/testFiles/grid.txt")
-
 words_to_find = ["asdf", "helloworld"]
-
 for word in words_to_find:
     if ws.is_present(word):
         print("found {}".format(word))
