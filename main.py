591e52bb3a09e9cb897a9a43b16ea6c16fde7e77
maru <masarushimoji@hotmail.com>
2024-10-19 19:48:56 +0100

    Algorithm works for rows and columns

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ class WordSearch(object):
     def __init__(self, grid):
         self.grid = grid
         self.fileContents = []
-        with open(self.grid, 'r') as file: 
-            for line in file:
-                self.fileContents.append(line)
+        with open(self.grid, 'r') as file:
+            for line in file: self.fileContents.append(line)
         self.fileContents = [s[:-1] for s in self.fileContents] 
         self.columns = len(self.fileContents[0])
         self.rows = len(self.fileContents)
@@ -19,13 +18,22 @@ class WordSearch(object):
             if word in line:
                 return True
         
-            
+        for x in range(self.columns):
+            s = ""
+            for y in range(self.rows):
+                s = s + (self.fileContents[y])[x]
+            print(s)
+            if word in s:
+                return True
+
+
+
         return False
 
   
 
 ws = WordSearch("/home/maru/Projects/pexipStuff/testFiles/grid.txt")
-words_to_find = ["asdf"]
+words_to_find = ["hi", "bonkers", "hello"]
 for word in words_to_find:
     if ws.is_present(word):
         print("found {}".format(word))
