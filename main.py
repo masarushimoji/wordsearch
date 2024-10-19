2caa6d3a0eaf5dcf4fc6d7b999976956270da7c9
maru <masarushimoji@hotmail.com>
2024-10-19 13:59:31 +0100

    Puts contents of textfile with the grid of letters into a list

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 class WordSearch(object):
     def __init__(self, grid):
         self.grid = grid
+        self.fileContents = []
+        rowLength = 10
+
+
+        with open(self.grid, 'r') as file:
+            self.fileContents = file.read().split()
+        
+
     def is_present(self, word):
-        return True
+        print(self.fileContents)
+        return False
+
+
+ws = WordSearch("/home/maru/Projects/pexipStuff/testFiles/grid.txt")
+
+words_to_find = ["asdf", "helloworld"]
+
+for word in words_to_find:
+    if ws.is_present(word):
+        print("found {}".format(word))
