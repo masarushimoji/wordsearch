1a751ec2d497e8c4474d898d3bde5c0d04a02793
masarushimoji <masarushimoji@hotmail.com>
2024-10-24 17:19:07 +0100

    Made columns list declared in the init of class

--- main.py
+++ main.py
@@ -2,25 +2,25 @@ class WordSearch(object):
     def __init__(self, grid):
         self.grid = grid
         self.ROW_LENGTH = 3
-        self.gridArr = []
+        self.rows = []
+        self.columns = ['' for _ in range(self.ROW_LENGTH)]
         
         for i in range(0, len(self.grid), self.ROW_LENGTH):
-            self.gridArr.append(self.grid[i:i+3])
-            
+            self.rows.append(self.grid[i:i+3])
+        
+        for x in range(self.ROW_LENGTH):
+            for y in range(self.ROW_LENGTH):
+                self.columns[x] += self.rows[y][x]
 
     def is_present(self, word):
-        # for loop that loops through the rows and tries to see if the word is
-        # in the grid horizonatally.
-        for line in self.gridArr:
+        for line in self.rows:
             if word in line:
                 return True # return true in the case that the word has been found
         
-        for x in range(self.ROW_LENGTH):
-            s = "" # declare an empty string variable s
-            for y in range(self.ROW_LENGTH):
-                s = s + (self.gridArr[y])[x] #loops through the "y"th column and concatenates the letters in each column into variable s
-            if word in s:
-                return True # return true in the case tht the word has been found
+        for line in self.columns:
+            if word in line:
+                return True
+        
         return False #word has not been found in a row or column so return false
 
 
@@ -35,6 +35,6 @@ words_to_find = ["hi", "abc", "aa"]
 
 for word in words_to_find:
     if ws.is_present(word):
-        print("found {}".format(word))
+        print("Found {}".format(word))
     else:
         print("HAVEN't found {}".format(word))
