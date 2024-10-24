3d90f05202fb0125aa18fce47b25b31a4e92e17b
masarushimoji <masarushimoji@hotmail.com>
2024-10-24 16:51:08 +0100

    Code is now functional again

--- main.py
+++ main.py
@@ -6,20 +6,19 @@ class WordSearch(object):
         
         for i in range(0, len(self.grid), self.ROW_LENGTH):
             self.gridArr.append(self.grid[i:i+3])
-        print(self.gridArr)
-        
+            
 
     def is_present(self, word):
         # for loop that loops through the rows and tries to see if the word is
         # in the grid horizonatally.
-        for line in self.fileContents:
+        for line in self.gridArr:
             if word in line:
                 return True # return true in the case that the word has been found
         
-        for x in range(self.columns):
+        for x in range(self.ROW_LENGTH):
             s = "" # declare an empty string variable s
-            for y in range(self.rows):
-                s = s + (self.fileContents[y])[x] #loops through the "y"th column and concatenates the letters in each column into variable s
+            for y in range(self.ROW_LENGTH):
+                s = s + (self.gridArr[y])[x] #loops through the "y"th column and concatenates the letters in each column into variable s
             if word in s:
                 return True # return true in the case tht the word has been found
         return False #word has not been found in a row or column so return false
@@ -32,8 +31,10 @@ with open(dir, 'r') as file:
     fileContents = file.readline().strip() 
     
 ws = WordSearch(fileContents)
-words_to_find = ["hi", "bonkers", "hello"]
+words_to_find = ["hi", "abc", "aa"]
 
 for word in words_to_find:
     if ws.is_present(word):
         print("found {}".format(word))
+    else:
+        print("HAVEN't found {}".format(word))
