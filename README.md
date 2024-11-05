ea16f98eefb2e9a751cdf7e82cf93f49dab19933
masarushimoji <masarushimoji@hotmail.com>
2024-11-05 13:07:46 +0000

    remove unnecessary code and made the words found list into a hashmap

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # wordsearch
 wordsearch is a simple python program that tries to find a word within a grid of letters from a-z either horizontally or vertically.
+# Optimisations made
+The substrings for the rows and columns are precomputed when the object is instatiated to ensure that this isn't done every time the object is used.
+ 
 # Side notes
+The code has taken around 8-10 hours as I had to refresh my understanding of python syntax before undertaking the task.
 Please note that all the testing for the code has been done on a machine running the latest release of arch linux.
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ class WordSearch(object):
         self.ROW_LENGTH = 100
         self.rows = []
         self.columns = ['' for _ in range(self.ROW_LENGTH)]
-        self.wordsFound = []
+        self.wordsFound = set()
         
         for i in range(0, len(self.grid), self.ROW_LENGTH):
             self.rows.append(self.grid[i:i+self.ROW_LENGTH])
@@ -15,15 +15,15 @@ class WordSearch(object):
 
 
     def is_present(self, word):
-        if word in wordsFound:
+        if word in  self.wordsFound:
             return True
         for line in self.rows:
             if word in line:
-                wordsFound.append(word)
+                self.wordsFound.add(word)
                 return True   
         for line in self.columns:
             if word in line:
-                wordsFound.append(word)
+                self.wordsFound.add(word)
                 return True
         return False 
 
@@ -35,13 +35,10 @@ with open(dir, 'r') as file:
     fileContents = file.readline().strip().lower()
 
 ws = WordSearch(fileContents)
-words_to_find = ["hi", "abc", "aa"]
-
-wordsFound = []
+words_to_find = ["hi", "abc", "aa", "computational"]
 
 for word in words_to_find:
     if ws.is_present(word):
         print("Found {}".format(word))
-        wordsFound.append(word)
     else:
         print("HAVEN't found {}".format(word))
